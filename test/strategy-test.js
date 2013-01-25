1608f78a2529eb976c80ad8fdfd6ef3286c6a24e
Jared Hanson <jaredhanson@gmail.com>
2013-01-24 19:59:47 -0800

    Wrap error for informative message.

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -106,6 +106,9 @@ vows.describe('DwollaStrategy').addBatch({
       'should error' : function(err, req) {
         assert.isNotNull(err);
       },
+      'should wrap error in InternalOAuthError' : function(err, req) {
+        assert.equal(err.constructor.name, 'InternalOAuthError');
+      },
       'should not load profile' : function(err, profile) {
         assert.isUndefined(profile);
       },
